5299a8eebf0aa0731b30373a40aa920d4f8d06cb
SonakshiSikhwal <42645633+SonakshiSikhwal@users.noreply.github.com>
2020-11-21 17:41:01 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,81 @@
+<html>
+<head>
+    <title>Basic Banking System</title>
+    <link rel="stylesheet" href="button.css">
+	<style>
+	button{
+		
+		  border: 4px;
+  color: black;
+  padding: 30px 50px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 30px;
+  margin: 4px 2px;
+  cursor: pointer;
+  font-family: "Lucida Console", Courier, monospace;
+		border-radius: 12px;
+	}
+	tr{
+		text-align:center;
+	}
+	body
+	{
+		text-align:center;
+	}
+		body{
+		background-image:url("bank.jpg");
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+	</style>
+	<style>
+.button {
+  border: none;
+  color: white;
+  padding: 30px 50px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 30px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+.button1 {color:#fff; background-color: Transparent; border: 5px solid #000;} 
+.button2 {color:#fff; background-color: Transparent; border: 5px solid #000;} 
+</style>
+</head>
+<body >
+<h1 class="alert alert-success rounded-0"><small class="float-right text-muted" style="font-size: 25pt;color:#fff"><kbd>PRESENTED BY : SONAKSHI SIKHWAL</kbd></small></h1>
+<br>
+	<div id="header">
+       <br>
+	   <br>
+	   <br>
+	   <br>
+	   <br>
+       <h2 style=" font-family:Agency FB; font-size: 55px;color:#000;"> SPARKS BANKING SYSTEM </h2>
+    </div>
+        <div id="section">
+            <table>
+                <tr></tr>
+                <tr><br>
+				<a href="getdetail.php">
+               <button class="button1" type="button" href="getdetail.php">CUSTOMER INFO</button>
+                </a>
+                </tr>
+				
+			   <tr>        
+               <br> <br> <br>
+			   <a href="transaction.php">
+			   <button class="button2" type="button">TRANSACTION DETAILS</button>
+               </a>
+               
+               </tr>
+            </table>
+    </div>
+</body>
+</html>
